08fdc93e0df4887e04e4d757670bd688d41c72d4
xiongzhangxyq <128204019+xiongzhangxyq@users.noreply.github.com>
2024-01-22 10:30:04 +0800

    Update prediction.py

--- somecode/prediction.py
+++ somecode/prediction.py
@@ -20,7 +20,7 @@ import pandas as pd
 from gat import GATNet
 
 warnings.filterwarnings('ignore')
-torch.random.manual_seed(420)
+torch.random.manual_seed(3407)
 ss = StandardScaler()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -60,8 +60,6 @@ in_c = 6
 hid_c = 8
 out_c = 6
 
-# 编解码部分超参
-
 # fusion weight 超参
 fin_c = 6
 fout_c = 1
@@ -72,7 +70,7 @@ CBGN = 2943
 
 
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # 配置GPU,因为可能有多个GPU，这里用了第0号GPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"  
     train_data = LoadData(data_path=["Data_features/cbg.pkl", "Data_features/cbg_omicro.pkl"], num_nodes=TallN,
                           divide_days=[42, 21],
                           time_interval=1, history_length=7, train_mode="train")
@@ -156,26 +154,19 @@ def main():
         total_loss = 0.0
         Test_loss = []
         for idex, data in enumerate(test_loader):
-            print("测试：",idex)
-            # 下面得到的预测结果是归一化的结果
             input = data["flow_x"].to(device)
             target = data["flow_y"].to(device)
             # attentionweight, graph = fusion(input.squeeze(2), idex)
-            my_net.zero_grad()  # 梯度清零
+            my_net.zero_grad()  
             predict_value = my_net(input, idex, device)
             loss = criterion(predict_value[:, 0:CBGN, :, 0:1], target[:, 0:CBGN, :, :])  # 使用MSE计算loss
             Test_loss.append(loss.item())
             total_loss += loss.item()
-            # 下面把预测值和目标值的batch放到第二维的时间维度，这是因为在测试数据的时候对样本没有shuffle，
-            # 所以每一个batch取出来的数据就是按时间顺序来的，因此放到第二维来表示时间是合理的.
             predict_value1 = predict_value[:, 0:CBGN, :, 0:1].transpose(0, 2).squeeze(0)  # [2943,16,1]
             target_value1 = target[:, 0:CBGN, :, :].transpose(0, 2).squeeze(0)  # [2943,16,1]
 
             performance1, data_to_save1 = compute_performance(predict_value1, target_value1,
-                                                              test_loader)  # 计算模型的性能，返回评价结果和恢复好的数据
-
-            # 下面这个是每一个batch取出的数据，按batch这个维度进行串联，最后就得到了整个时间的数据，也就是
-            # [N, T, D] = [N, T1+T2+..., D]
+                                                              test_loader) 
             Predict1 = np.concatenate([Predict1, data_to_save1[0]], axis=1)
             Target1 = np.concatenate([Target1, data_to_save1[1]], axis=1)
 
@@ -204,32 +195,21 @@ def main():
 
 
 def compute_performance(prediction, target, data):  # 计算模型性能
-    # 下面的try和except实际上在做这样一件事：当训练+测试模型的时候，数据肯定是经过dataloader的，所以直接赋值就可以了
-    # 但是如果将训练好的模型保存下来，然后测试，那么数据就没有经过dataloader，是dataloader型的，需要转换成dataset型。
     try:
         dataset = data.dataset
     except:
         dataset = data
-
-    # 对预测和目标数据进行逆归一化,flow_norm为归一化的基，flow_norm[0]为最大值，flow_norm[1]为最小值
     print("prediction：", prediction.size())
-
     prediction = LoadData.recover_data(dataset.flow_norm[0][0:CBGN, :, :], dataset.flow_norm[1][0:CBGN, :, :],
                                        prediction.data.cpu().numpy())
     target = LoadData.recover_data(dataset.flow_norm[0][0:CBGN, :, :], dataset.flow_norm[1][0:CBGN, :, :],
                                    target.data.cpu().numpy())  # [5886,16,1]
-
     mae, mape, rmse = Evaluation.total(target.reshape(-1), prediction.reshape(-1))  # 变成常向量才能计算这三种指标
-
     performance = [mae, mape, rmse]
     recovered_data = [prediction, target]
 
     return performance, recovered_data  # 返回评价结果，以及恢复好的数据（为可视化准备的）
 
-
-
-
-print("nihao")
 if __name__ == '__main__':
     main()
     # 可视化
